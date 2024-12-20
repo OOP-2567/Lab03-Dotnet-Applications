dd8eed0bfa5013555c42843debc79f469eb25308
jamelswift <Wipatsasicha0702@gmail.com>
2024-12-20 13:02:52 +0700

    Add files via upload

---
+++ HelloWorld-Lab03/FormAdd1/Form1.cs
@@ -0,0 +1,17 @@
+namespace FormAdd1;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+    private void buttonAdd_Click(object sender, EventArgs e)
+    {
+        double a, b, c;
+        double.TryParse(textBox_A.Text, out a);
+        double.TryParse(textBox_B.Text, out b);
+        c = a + b;
+        textBox_C.Text = c.ToString();
+    }
+}
